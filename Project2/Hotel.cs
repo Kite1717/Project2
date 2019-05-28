c7a09034b123062d86e0935ba65f80dfabfc0f29
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2019-05-28 16:12:45 +0300

    Update Hotel.cs

--- Project2/Hotel.cs
+++ Project2/Hotel.cs
@@ -12,9 +12,9 @@ namespace Project2
     abstract class Hotel
     {
         protected string city { get; }
-        protected int star { get; }
-        protected List<Room> rooms;
-
+        public int star { get; }
+        public List<Room> rooms { get; }
+        public int roomCapacity { get; }
         /// <summary>
         /// 
         /// </summary>
@@ -22,17 +22,44 @@ namespace Project2
         /// <param name="star">the number of stars that you have</param>
         public Hotel(string city, int star)
         {
+            
             this.city = city;
             this.star = star;
             this.rooms = new List<Room>();
+            Random random = new Random();
+            switch(star)
+            {
+                case 1:
+                    {
+                        roomCapacity = random.Next(7, 11);
+                        break;
+                    }
+                case 2:
+                    {
+                        roomCapacity = random.Next(10, 16);
+                        break;
+                    }
+                case 3:
+                    {
+                        roomCapacity = random.Next(15, 21);
+                        break;
+                    }
+                case 4:
+                    {
+                        roomCapacity = random.Next(20, 26);
+                        break;
+                    }
+                case 5:
+                    {
+                        roomCapacity = random.Next(25, 31);
+                        break;
+                    }
+                           
+            }
 
         }
 
-        /// <summary>
-        ///  according to the structure of the hotel adds to the room
-        /// </summary>
-        /// <returns>to be successful control</returns>
-        public abstract bool addRoom();
+       
 
         /// <summary>
         /// for saving information when closing the application
