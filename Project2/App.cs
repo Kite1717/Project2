536bcb8fdc560d6d0f58a43486c616b20b9b518d
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2019-05-28 16:09:25 +0300

    Update App Class

--- Project2/App.cs
+++ Project2/App.cs
@@ -30,11 +30,292 @@ namespace Project2
         }
         /// <summary>
         /// auto adds hotels , admin and start sing up/in screen
+        /// default 30 otel 
         /// </summary>
         public void startDemo()
         {
+            Random random = new Random();
+            int k;
+            string type = null;
+            string[] data = { "ISTANBUL", "CANAKKALE", "IZMIR", "KOCAELI", "ANKARA", "ANTALYA", "MUGLA", "VAN", "KARS", "ESKISEHIR" };
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    
+                    k = random.Next(1, 6);
+                    type = null;
+                
+                    switch (k)
+                    {
+                        case 1:
+                            {
+                                type = "Hostel";
+                                break;
+
+                            }
+                        case 2:
+                            {
+                                type = "BoutiqueHotel";
+                                break;
+
+                            }
+                        case 3:
+                            {
+                                type = "AllInHotel";
+                                break;
+
+                            }
+                        case 4:
+                            {
+                                type = "HolidayVillage";
+                                break;
+
+                            }
+                        case 5:
+                            {
+                                type = "UltraLuxuryHotel";
+                                break;
+
+                            }
+                    }
+                    hotels.Add(HotelFactory.GetHotel(type, data[i]));
+
+
+                
+                }
+              
+
+            }
+            for (int i = 0; i < hotels.Count; i++)
+            {
+                hotels
+            }
+
+
+           
+
             //add hotels
             loginScreen();
+        }
+
+        private void addRoom(Hotel hotel)
+        {
+            switch(hotel.star)
+            {
+                case 1:
+                    {                       
+                        int num = 100;
+                        for (int i = 0; i < hotel.roomCapacity / 2; i++)
+                        {
+                            num++;
+                           hotel. rooms.Add(RoomFactory.GetRoom("SingleBedRoom", num));
+                        }
+                        num = 200;
+                        for (int i = 0; i < hotel.roomCapacity - hotel.roomCapacity / 2; i++)
+                        {
+                            num++;
+                           hotel.rooms.Add(RoomFactory.GetRoom("TwoBedRoom", num));
+                        }
+                        //wrong room name
+                        try
+                        {
+                            hotel.rooms.Add(RoomFactory.GetRoom("Tsadasm", 518));
+                        }
+                        catch (ArgumentException e)
+                        {
+
+                        }
+
+                        break;
+
+                    }
+                case 2:
+                    {
+                      
+                        int num = 100;
+                        for (int i = 0; i < hotel.roomCapacity / 3; i++)
+                        {
+                            num++;
+                           hotel.rooms.Add(RoomFactory.GetRoom("SingleBedRoom", num));
+                        }
+                        num = 200;
+                        for (int i = 0; i < hotel.roomCapacity / 3; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("TwoBedRoom", num));
+                        }
+
+                        num = 300;
+                        for (int i = 0; i < hotel.roomCapacity - ((hotel.roomCapacity / 3) * 2); i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("TwinRoom", num));
+                        }
+
+                        //null room name
+                        try
+                        {
+                            hotel.rooms.Add(RoomFactory.GetRoom(null, 312));
+                        }
+                        catch (ArgumentException e)
+                        {
+
+                        }
+                        break;
+
+                    }
+                case 3:
+                    {
+                        int num = 100;
+                        for (int i = 0; i < hotel.roomCapacity / 4; i++)
+                        {
+                            num++;
+                           hotel.rooms.Add(RoomFactory.GetRoom("SingleBedRoom", num));
+                        }
+                        num = 200;
+                        for (int i = 0; i < hotel.roomCapacity / 4; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("TwoBedRoom", num));
+                        }
+
+                        num = 300;
+                        for (int i = 0; i < hotel.roomCapacity / 4; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("TwinRoom", num));
+                        }
+
+
+                        num = 400;
+                        for (int i = 0; i < hotel.roomCapacity - ((hotel.roomCapacity / 4) * 3); i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("ThreeBedRoom", num));
+                        }
+
+                        //wrong room name
+                        try
+                        {
+                            hotel.rooms.Add(RoomFactory.GetRoom("adsadasda", 426));
+                        }
+                        catch (ArgumentException e)
+                        {
+
+                        }
+                        break;
+
+                    }
+                case 4:
+                    {
+                        int num = 100;
+                        for (int i = 0; i < hotel.roomCapacity / 5; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("SingleBedRoom", num));
+                        }
+                        num = 200;
+                        for (int i = 0; i < hotel.roomCapacity / 5; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("TwoBedRoom", num));
+                        }
+
+                        num = 300;
+                        for (int i = 0; i < hotel.roomCapacity / 5; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("TwinRoom", num));
+                        }
+
+
+                        num = 400;
+                        for (int i = 0; i < hotel.roomCapacity / 5; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("ThreeBedRoom", num));
+                        }
+
+                        num = 500;
+                        for (int i = 0; i < hotel.roomCapacity - ((hotel.roomCapacity / 5) * 4); i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("FamilyRoom", num));
+                        }
+
+                        //null room name
+                        try
+                        {
+                            hotel.rooms.Add(RoomFactory.GetRoom(null, 718));
+                        }
+                        catch (ArgumentException e)
+                        {
+
+                        }
+                        break;
+
+                    }
+                case 5:
+                    {
+                        int num = 100;
+                        for (int i = 0; i < hotel.roomCapacity / 6; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("SingleBedRoom", num));
+                        }
+                        num = 200;
+                        for (int i = 0; i < hotel.roomCapacity / 6; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("TwoBedRoom", num));
+                        }
+
+                        num = 300;
+                        for (int i = 0; i < hotel.roomCapacity / 6; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("TwinRoom", num));
+                        }
+
+
+                        num = 400;
+                        for (int i = 0; i < hotel.roomCapacity / 6; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("ThreeBedRoom", num));
+                        }
+
+                        num = 500;
+                        for (int i = 0; i < hotel.roomCapacity / 6; i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("FamilyRoom", num));
+                        }
+
+                        num = 600;
+                        for (int i = 0; i < hotel.roomCapacity - ((hotel.roomCapacity / 6) * 5); i++)
+                        {
+                            num++;
+                            hotel.rooms.Add(RoomFactory.GetRoom("KingRoom", num));
+                        }
+
+
+                        //wrong room name
+                        try
+                        {
+                            hotel.rooms.Add(RoomFactory.GetRoom("mustafa", 333));
+                        }
+                        catch (ArgumentException e)
+                        {
+
+                        }
+
+                        break;
+
+                    }
+            }
+
         }
 
         /// <summary>
