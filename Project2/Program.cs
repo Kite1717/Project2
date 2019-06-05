f6c90aef382425780e2b0f73cbcb625387229135
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2019-06-06 00:01:39 +0300

    Add console hide process

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Project2
 {
     class Program
     {
+
+        [DllImport("kernel32.dll")]
+       static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
         static void Main(string[] args)
         {
-            new App().startDemo();
+           /* var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+             */
+            //new App();
+            LoginScreen lg = new LoginScreen();
+            Application.Run(lg);
+
+
+
         }
+
+     
     }
 }
+
+
