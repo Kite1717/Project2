9fa6947f247ced0450ef055d666d64fec800b9eb
bkansu <batuhan.kansu@ogr.deu.edu.tr>
2019-06-12 16:28:38 +0300

    Add LoginScreen.cs

--- Project2/UI/LoginScreen.Designer.cs
+++ Project2/UI/LoginScreen.Designer.cs
@@ -34,7 +34,6 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label4 = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.label6 = new System.Windows.Forms.Label();
@@ -44,6 +43,12 @@
             this.richTextBox5 = new System.Windows.Forms.RichTextBox();
             this.label9 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.richPassword1 = new Project2.RichPassword();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -98,116 +103,204 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(477, 131);
+            this.label4.Location = new System.Drawing.Point(545, 116);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(126, 18);
             this.label4.TabIndex = 5;
             this.label4.Text = "User Name (ID)";
+            this.label4.Visible = false;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(480, 151);
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(545, 146);
+            this.richTextBox1.MaxLength = 15;
+            this.richTextBox1.Multiline = false;
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(208, 39);
+            this.richTextBox1.Size = new System.Drawing.Size(208, 40);
             this.richTextBox1.TabIndex = 6;
             this.richTextBox1.Text = "";
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Location = new System.Drawing.Point(480, 213);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(208, 39);
-            this.richTextBox2.TabIndex = 8;
-            this.richTextBox2.Text = "";
+            this.richTextBox1.Visible = false;
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(477, 193);
+            this.label5.Location = new System.Drawing.Point(545, 202);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(83, 18);
             this.label5.TabIndex = 7;
             this.label5.Text = "Password";
+            this.label5.Visible = false;
             // 
             // richTextBox3
             // 
-            this.richTextBox3.Location = new System.Drawing.Point(480, 300);
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox3.Location = new System.Drawing.Point(545, 321);
+            this.richTextBox3.MaxLength = 30;
             this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(208, 39);
+            this.richTextBox3.Size = new System.Drawing.Size(208, 40);
             this.richTextBox3.TabIndex = 10;
             this.richTextBox3.Text = "";
+            this.richTextBox3.Visible = false;
+            this.richTextBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox3_KeyPress);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.Location = new System.Drawing.Point(477, 279);
+            this.label6.Location = new System.Drawing.Point(545, 291);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(52, 18);
             this.label6.TabIndex = 9;
             this.label6.Text = "Name";
+            this.label6.Visible = false;
             // 
             // richTextBox4
             // 
-            this.richTextBox4.Location = new System.Drawing.Point(480, 363);
+            this.richTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox4.Location = new System.Drawing.Point(545, 410);
+            this.richTextBox4.MaxLength = 30;
             this.richTextBox4.Name = "richTextBox4";
-            this.richTextBox4.Size = new System.Drawing.Size(208, 39);
+            this.richTextBox4.Size = new System.Drawing.Size(208, 40);
             this.richTextBox4.TabIndex = 12;
             this.richTextBox4.Text = "";
+            this.richTextBox4.Visible = false;
+            this.richTextBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox4_KeyPress);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label7.Location = new System.Drawing.Point(477, 342);
+            this.label7.Location = new System.Drawing.Point(542, 377);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(75, 18);
             this.label7.TabIndex = 11;
             this.label7.Text = "Surname";
+            this.label7.Visible = false;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label8.Location = new System.Drawing.Point(477, 405);
+            this.label8.Location = new System.Drawing.Point(542, 472);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(36, 18);
             this.label8.TabIndex = 13;
             this.label8.Text = "Age";
+            this.label8.Visible = false;
             // 
             // richTextBox5
             // 
-            this.richTextBox5.Location = new System.Drawing.Point(480, 426);
+            this.richTextBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox5.Location = new System.Drawing.Point(545, 502);
+            this.richTextBox5.MaxLength = 2;
             this.richTextBox5.Name = "richTextBox5";
-            this.richTextBox5.Size = new System.Drawing.Size(208, 39);
+            this.richTextBox5.Size = new System.Drawing.Size(208, 40);
             this.richTextBox5.TabIndex = 14;
             this.richTextBox5.Text = "";
+            this.richTextBox5.Visible = false;
+            this.richTextBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox5_KeyPress);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label9.Location = new System.Drawing.Point(541, 90);
+            this.label9.Location = new System.Drawing.Point(581, 80);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(82, 25);
+            this.label9.Size = new System.Drawing.Size(0, 25);
             this.label9.TabIndex = 15;
-            this.label9.Text = "asdsad";
+            this.label9.Visible = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(546, 498);
+            this.button1.Location = new System.Drawing.Point(600, 563);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 56);
+            this.button1.Size = new System.Drawing.Size(89, 57);
             this.button1.TabIndex = 16;
-            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(169, 146);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(335, 34);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "must be at least 6 characters and must not be blank\r\nonly as special characters  " +
+    ".  _ - ";
+            this.label10.Visible = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(118, 321);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(386, 34);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "must be at least 3 characters ,\r\n must not be blank and must contain letters and " +
+    "spaces only";
+            this.label11.Visible = false;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(311, 508);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(193, 17);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "must be over 18 years of age";
+            this.label12.Visible = false;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox2.Location = new System.Drawing.Point(545, 238);
+            this.richTextBox2.MaxLength = 15;
+            this.richTextBox2.Multiline = false;
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(208, 40);
+            this.richTextBox2.TabIndex = 23;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.Visible = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label13.ForeColor = System.Drawing.Color.Red;
+            this.label13.Location = new System.Drawing.Point(779, 244);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(54, 20);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "Show";
+            this.label13.Visible = false;
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // richPassword1
+            // 
+            this.richPassword1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richPassword1.Location = new System.Drawing.Point(545, 238);
+            this.richPassword1.Name = "richPassword1";
+            this.richPassword1.Size = new System.Drawing.Size(208, 40);
+            this.richPassword1.TabIndex = 22;
+            this.richPassword1.Text = "";
+            this.richPassword1.Visible = false;
             // 
             // LoginScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richPassword1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.richTextBox5);
@@ -216,13 +309,14 @@
             this.Controls.Add(this.label7);
             this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.groupBox1);
             this.Name = "LoginScreen";
             this.Text = "LoginScreen";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoginScreen_FormClosing);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -238,7 +332,6 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.RichTextBox richTextBox3;
         private System.Windows.Forms.Label label6;
@@ -248,5 +341,11 @@
         private System.Windows.Forms.RichTextBox richTextBox5;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private RichPassword richPassword1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label13;
     }
 }
